990bdd6a445e36cc97d7859e18f324d419314f26
Dan Lecocq <dan@seomoz.org>
2013-01-06 12:18:53 -0800

    Refactoring to remove global cache.
    Now there are individual cache objects that can have their own caching
    policies, cache clearing, etc. Functionally, they work much like the
    old `reppy` module.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Copyright (c) 2011 SEOmoz
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -9,10 +9,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -22,22 +22,23 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 try:
-	from setuptools import setup
-	extra = {
-		'install_requires' : ['python-dateutil==1.5']
-	}
+    from setuptools import setup
+    extra = {
+        'install_requires': ['python-dateutil==1.5', 'url', 'requests']
+    }
 except ImportError:
-	from distutils.core import setup
-	extra = {
-		'dependencies' : ['python-dateutil==1.5']
-	}
+    from distutils.core import setup
+    extra = {
+        'dependencies': ['python-dateutil==1.5', 'url', 'requests']
+    }
 
 setup(
-	name             = 'reppy',
-	version          = '0.1.0',
-	description      = 'Replacement robots.txt Parser',
-	long_description = '''Replaces the built-in robotsparser with a RFC-conformant implementation
-that supports modern robots.txt constructs like Sitemaps, Allow, and Crawl-delay. Main features:
+    name             = 'reppy',
+    version          = '0.2.0',
+    description      = 'Replacement robots.txt Parser',
+    long_description = '''Replaces the built-in robotsparser with a
+RFC-conformant implementation that supports modern robots.txt constructs like
+Sitemaps, Allow, and Crawl-delay. Main features:
 
 - Memoization of fetched robots.txt
 - Expiration taken from the `Expires` header
@@ -45,18 +46,18 @@ that supports modern robots.txt constructs like Sitemaps, Allow, and Crawl-delay
 - Configurable user agent for fetching robots.txt
 - Automatic refetching basing on expiration
 ''',
-	author           = 'Dan Lecocq',
-	author_email     = 'dan@seomoz.org',
-	url              = 'http://github.com/seomoz/reppy',
-	packages         = ['reppy'],
-	license          = 'MIT',
-	platforms        = 'Posix; MacOS X',
-	test_suite       = 'tests.testReppy',
-	classifiers      = [
-		'License :: OSI Approved :: MIT License',
-		'Development Status :: 3 - Alpha',
-		'Environment :: Web Environment',
-		'Intended Audience :: Developers',
-		'Topic :: Internet :: WWW/HTTP'],
-	**extra
+    author           = 'Dan Lecocq',
+    author_email     = 'dan@seomoz.org',
+    url              = 'http://github.com/seomoz/reppy',
+    packages         = ['reppy'],
+    license          = 'MIT',
+    platforms        = 'Posix; MacOS X',
+    test_suite       = 'tests.testReppy',
+    classifiers      = [
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP'],
+    **extra
 )
