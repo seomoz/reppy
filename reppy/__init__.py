c4eed0418e82f93b0669e9904f0ee3feffa05b9c
David Barts <dbarts@seomoz.org>
2013-10-01 14:21:21 -0700

    Use default ttl if Expires: is mangled, not 0.

--- reppy/__init__.py
+++ reppy/__init__.py
@@ -130,7 +130,7 @@ class Utility(object):
                 try:
                     base = Utility.parse_time(date)
                 except ValueError:
-                    base = time.time()
+                    return default
             try:
                 return Utility.parse_time(expires) - base
             except ValueError:
