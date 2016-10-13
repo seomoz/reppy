dc224cf169a90feb02dd61db90f6d861af217375
Dan Lecocq <dan@moz.com>
2016-10-13 08:38:24 -0700

    Failing tests about util.parse_date.

--- tests/test_util.py
+++ tests/test_util.py
@@ -66,3 +66,27 @@ class PairsTest(unittest.TestCase):
             ('hello', 'world')
         ]
         self.assertEqual(lines, list(util.pairs(content)))
+
+
+class ParseDateTest(unittest.TestCase):
+    '''Tests about parsing dates provided in headers.'''
+
+    def test_invalid(self):
+        '''Raises an error on an unparseable date.'''
+        with self.assertRaises(ValueError):
+            util.parse_date('Not a real date')
+
+    def test_imf_fixdate(self):
+        '''Successfully parses a IMF fixdate.'''
+        self.assertEqual(
+            util.parse_date('Sun, 06 Nov 1994 08:49:37 GMT'), 784111777)
+
+    def test_rfc_850(self):
+        '''An obsolete date format that is also supported.'''
+        self.assertEqual(
+            util.parse_date('Sunday, 06-Nov-94 08:49:37 GMT'), 784111777)
+
+    def test_asctime(self):
+        '''An obsolete date format that is also supported.'''
+        self.assertEqual(
+            util.parse_date('Sun Nov  6 08:49:37 1994'), 784111777)
