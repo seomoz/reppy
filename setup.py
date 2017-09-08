c245fedefa6586fc3eaff10700b01f497f287519
Brandon Forehand <brandon@moz.com>
2017-09-08 15:28:58 -0700

    Release version 0.4.8.

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ ext_modules = [
 
 setup(
     name='reppy',
-    version='0.4.7',
+    version='0.4.8',
     description='Replacement robots.txt Parser',
     long_description='''Replaces the built-in robotsparser with a
 RFC-conformant implementation that supports modern robots.txt constructs like
